c0c8d7b87896c359d3f1e85a9e547b329e9df3a2
Sarah Young <y.sarahc@gmail.com>
2014-05-13 08:48:31 +0100

    Divisible by 5 passing test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def divisible_by_3?(number)
 	number % 3 == 0	
 end
+
+def divisible_by_5?(number)
+	number % 5 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,3 +6,9 @@ describe "divisible by three" do
 		expect(divisible_by_3?(3)).to be_true
 	end
 end
+
+describe "divisible by five" do 
+	it 'should know if a number is divisible by five' do 
+		expect(divisible_by_5?(5)).to be_true
+	end
+end
